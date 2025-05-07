c1da7755aede779a74d38a03cba1256dcdd462ba
managanemeke <managanemeke@gmail.com>
2025-05-07 17:20:28 +0500

    refactor: delete redundant comments

--- src/js/main/shared/aeft.ts
+++ src/js/main/shared/aeft.ts
@@ -1,26 +1,11 @@
 import {evalES} from "../../lib/utils/bolt";
 
-/**
- * {@link src/jsx/aeft/test.ts}
- */
 export const test = async (): Promise<void> => {await evalES("test()")};
 
-/**
- * {@link src/jsx/aeft/test.ts}
- */
 export const notify = async (text: string): Promise<void> => {await evalES(`notify("${text}")`)};
 
-/**
- * {@link src/jsx/aeft/placeSaveMarkers.ts}
- */
 export const placeSaveMarkers = async (): Promise<void> => {await evalES("placeSaveMarkers()")};
 
-/**
- * {@link src/jsx/aeft/saveSubstrates.ts}
- */
 export const saveSubstrates = async (): Promise<void> => {await evalES("saveSubstrates()")};
 
-/**
- * {@link src/jsx/aeft/deleteSaveMarkers.ts}
- */
 export const deleteSaveMarkers = async (): Promise<void> => {await evalES("deleteSaveMarkers()")};
