18d3edbec44eca34b08d2f0e69307cf099d7f350
managanemeke <managanemeke@gmail.com>
2025-05-07 17:23:22 +0500

    refactor: escape from oneliners

--- src/js/main/shared/aeft.ts
+++ src/js/main/shared/aeft.ts
@@ -1,11 +1,21 @@
 import {evalES} from "../../lib/utils/bolt";
 
-export const test = async (): Promise<void> => {await evalES("test()")};
+export const test = async (): Promise<void> => {
+  await evalES("test()");
+};
 
-export const notify = async (text: string): Promise<void> => {await evalES(`notify("${text}")`)};
+export const notify = async (text: string): Promise<void> => {
+  await evalES(`notify("${text}")`);
+};
 
-export const placeSaveMarkers = async (): Promise<void> => {await evalES("placeSaveMarkers()")};
+export const placeSaveMarkers = async (): Promise<void> => {
+  await evalES("placeSaveMarkers()");
+};
 
-export const saveSubstrates = async (): Promise<void> => {await evalES("saveSubstrates()")};
+export const saveSubstrates = async (): Promise<void> => {
+  await evalES("saveSubstrates()");
+};
 
-export const deleteSaveMarkers = async (): Promise<void> => {await evalES("deleteSaveMarkers()")};
+export const deleteSaveMarkers = async (): Promise<void> => {
+  await evalES("deleteSaveMarkers()");
+};
